4dca738b6ebb39ed535c7df2c985e7e51d9a1de7
saori.miyajima <sor.1015.0302@gmail.com>
2020-06-04 17:43:17 +0900

    Add Employee create

--- src/models/validators/EmployeeValidator.java
+++ src/models/validators/EmployeeValidator.java
@@ -40,7 +40,7 @@ public class EmployeeValidator {
         // すでに登録されている社員番号との重複チェック
         if(code_duplicate_check_flag) {
             EntityManager em = DBUtil.createEntityManager();
-            long employee_count = (long)em.createNamedQuery("checkregisteredCode", Long.class)
+            long employee_count = (long)em.createNamedQuery("checkRegisteredCode", Long.class)
                                           .setParameter("code", code)
                                           .getSingleResult();
             em.close();
