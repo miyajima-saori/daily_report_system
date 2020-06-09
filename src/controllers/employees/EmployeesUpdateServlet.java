d3baec8d7342656dd84877f1b76fed2ffd75eff3
saori.miyajima <sor.1015.0302@gmail.com>
2020-06-09 18:31:44 +0900

    Add Employee update

--- src/controllers/employees/EmployeesUpdateServlet.java
+++ src/controllers/employees/EmployeesUpdateServlet.java
@@ -79,7 +79,7 @@ public class EmployeesUpdateServlet extends HttpServlet {
                 request.setAttribute("employee", e);
                 request.setAttribute("errors", errors);
 
-                RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/employees/edit.jsp");
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/edit.jsp");
                 rd.forward(request, response);
             } else {
                 em.getTransaction().begin();
